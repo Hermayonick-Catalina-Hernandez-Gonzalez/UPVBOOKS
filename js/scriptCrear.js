7ea8fdaa6f750299adfd9a5a26fb94840551ba1c
Yatzari Pecina <2130039@example.com>
2024-04-14 15:37:55 -0600

    unos pedillos

--- js/scriptCrear.js
+++ js/scriptCrear.js
@@ -1,22 +1,43 @@
-document.getElementById('foto').addEventListener('change', function(event) {
+document.getElementById('foto').addEventListener('change', function (event) {
   const file = event.target.files[0];
   const fotoPreview = document.querySelector('.foto-preview');
   if (file) {
-      const reader = new FileReader();
-      reader.onload = function(e) {
-          fotoPreview.style.backgroundImage = `url(${e.target.result})`;
-          fotoPreview.style.backgroundSize = 'cover';
-      };
-      reader.readAsDataURL(file);
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      fotoPreview.style.backgroundImage = `url(${e.target.result})`;
+      fotoPreview.style.backgroundSize = 'cover';
+    };
+    reader.readAsDataURL(file);
   } else {
-      fotoPreview.style.backgroundImage = '';
+    fotoPreview.style.backgroundImage = '';
   }
 });
 
-document.getElementById('seleccionar-foto').addEventListener('click', function() {
+document.getElementById('seleccionar-foto').addEventListener('click', function () {
   document.getElementById('foto').click();
 });
 
-document.getElementById('formCrear').addEventListener("submit", function(e){
+document.getElementById('formCrear').addEventListener("submit", function (e) {
   e.preventDefault();
+  var formData = new FormData(this);
+
+  fetch('../php/guardar_archivo.php', {
+    method: 'POST',  // Usa el método POST
+    body: formData,  // Envía el objeto FormData como cuerpo de la solicitud
+  }).then(response => {
+    if (!response.ok) {
+      // Si el estado HTTP no es OK, lanza un error con el estado y el texto de la respuesta
+      return response.text().then(text => {
+        throw new Error(`HTTP error ${response.status}: ${text}`);
+      });
+    }
+    return response.json();
+  })
+    .then(data => {
+      // Maneja la respuesta JSON
+    })
+    .catch(error => {
+      // Maneja cualquier error que pueda ocurrir durante la solicitud
+      console.error('Error:', error);
+    });
 });
--- php/guardar_archivo.php
+++ php/guardar_archivo.php
@@ -2,7 +2,7 @@
 
 // import de los archivos de código necesarios para la ejecución de este PHP file
 require "./config.php";  // Configuraciones generales de la aplicación
-require "./sesion.php";  // para acceder a las variables de sesión
+require "./sesion_requerida.php";  // para acceder a las variables de sesión
 require "./connection.php";  // para el acceso a datos
 
 // Indicamos que la respuesta es de tipo JSON, porque la petición a esta ejecución
@@ -45,9 +45,9 @@ $extension = strtolower($nombreArchivoParts[count($nombreArchivoParts) - 1]);
 $tamaño = $archivoSubido["size"];  // tamaño del archivo subido
 
 // Validación del tipo de archivo que se subió, que su extensión corresponda a
-// algún tipo de archivo de imagen o archivo PDF
-if (!in_array($extension, $EXT_ARCHIVOS_FOTOS) && $extension != "pdf") {
-    $errores[] = "Tipo de archivo no válido, solo se admiten imágenes y PDFs";
+// algún tipo de archivo de imagen
+if (!in_array($extension, $EXT_ARCHIVOS_FOTOS)) {
+    $errores[] = "Tipo de archivo no válido, solo se admiten imágenes";
     echo json_encode(["errores" => $errores]);
     exit();  // fin de la ejecución de este archivo PHP
 }
@@ -55,7 +55,7 @@ if (!in_array($extension, $EXT_ARCHIVOS_FOTOS) && $extension != "pdf") {
 // Generamos un nombre de archivo random (que para este caso será un número
 // hexadecimal) de longitud de 64 chars según 32 bytes random
 $nombreArchivoGuardado = strtoupper(bin2hex(random_bytes(32)));
-$ruta = "C:/xpp/htdocs/xampp/InstagramWEB/fotos/" . $nombreArchivoGuardado;  // ruta donde se guardará el archivo
+$ruta = "C:/xpp/htdocs/xampp/InstagramWEB/fotos/" . $nombreArchivoGuardado . "." . $extension;  // ruta donde se guardará el archivo
 $seGuardo = move_uploaded_file($archivoSubido["tmp_name"], $ruta);
 
 // Si no se guardo el archivo, regresamos un error
@@ -75,38 +75,21 @@ $fechaSubido = $now->format("Y-m-d H:i:s");
 
 // Ejecutamos la operación de insert del registro del archivo en DB
 $sqlCmd =  // Sentencia SQL del INSERT
-    "INSERT INTO archivos " .
-    "    (nombre_archivo, extension, nombre_archivo_guardado, tamaño," .
-    "     hash_sha256, fecha_subido, usuario_subio_id, descripcion)" .
-    "  VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+    "INSERT INTO fotos (secure_id, usuario_subio_id, nombre_archivo, tamaño, descripcion, fecha_subido) VALUES (?, ?, ?, ?, ?, ?)";
 $sqlParam = [  // array con los datos a guardar, según los placeholders '?'
-    $nombreArchivo, $extension, $nombreArchivoGuardado, $tamaño,
-    $hashSha256, $fechaSubido, $USUARIO_ID, $descripcion
+    $nombreArchivoGuardado, $USUARIO_ID, $nombreArchivo, $tamaño, $descripcion, $fechaSubido
 ];
 
 $stmt = $connection->prepare($sqlCmd);  // obtenemos el statement de la ejecución
-$stmt->execute($sqlParam);  // ejecutamos el statement con los parámetros
+if (!$stmt->execute($sqlParam)) {
+    $errores[] = "ERROR al añadir la consulta";
+    echo json_encode(["errores" => $errores]);
+    exit();  // fin de la ejecución de este archivo PHP
+}  // ejecutamos el statement con los parámetros
 
 // Obtenemos el id del registro que insertamos en tabla archivos, dado que 
 // este se calcula en DB al ser autoincrement
 $id = (int)$db->lastInsertId();  // la función regresa string, convertimos a int
-
-// Se guarda la operación de guardar como un registro en tabla
-// archivos_log_general
-$sqlCmd =  // Sentencia SQL del Insert en archivos_log_general
-    "INSERT INTO archivos_log_general " .
-    "    (archivo_id, usuario_id, fecha_hora, accion_realizada, " .
-    "     ip_realiza_operacion)" .
-    "  VALUES (?, ?, ?, ?, ?)";
-$accionRealizada = "GUARDAR";
-$sqlParam = [  // array con los datos a guardar, según los placeholders '?'
-    $id, $USUARIO_ID, $fechaSubido, $accionRealizada, $REQUEST_IP_ADDRESS
-];
-$stmt = $db->prepare($sqlCmd);  // obtenemos el statement de la ejecución
-$stmt->execute($sqlParam);  // ejecutamos el statement con los parámetros
-
-// Obtenemos el id del registro insertado en archivos_log_general, por si lo ocupamos
-$idEnLog = (int)$db->lastInsertId();  // la función regresa string, convertimos a int
 
 // Regresamos una respuesta como un JSON string, aquí pueden ir los diferentes datos que
 // pudieramos necesitar en el front-end.
@@ -123,4 +106,5 @@ $resObj = [
         "hashSha256" => $hashSha256
     ]
 ];
+
 echo json_encode($resObj);  // JSON a regresar como respuesta
